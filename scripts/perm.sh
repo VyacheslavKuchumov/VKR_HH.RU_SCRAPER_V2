2ed82745177f41da588f2a448d47f03713e6500f
VyacheslavKuchumov <Kuchumov76@mail.ru>
2025-03-04 12:57:49 +0500

    Update perm.sh

--- scripts/perm.sh
+++ scripts/perm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /home/vyachik/Documents/GitHub/VKR_HH.RU_SCRAPER_V2/main.py "Пермский край"
+/home/vyachik/Documents/GitHub/VKR_HH.RU_SCRAPER_V2/.venv/bin/python /home/vyachik/Documents/GitHub/VKR_HH.RU_SCRAPER_V2/main.py "Пермский край"
